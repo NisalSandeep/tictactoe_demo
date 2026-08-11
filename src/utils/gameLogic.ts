78f09e7a93885613932dae72d41385b0f3376418
Nisal Sandeep <sandeep-as22018@stu.kln.ac.lk>
2026-08-11 14:53:45 +0530

    fix: correct comment for middle columns in game logic

--- src/utils/gameLogic.ts
+++ src/utils/gameLogic.ts
@@ -10,7 +10,7 @@ export function calculateWinner(squares: Array<string | null>) {
     [3, 4, 5], // middle row
     [6, 7, 8], // bottom row
     [0, 3, 6], // left column
-    [1, 4, 7], // middle column
+    [1, 4, 7], // middle columns
     [2, 5, 8], // right column
     [0, 4, 8], // diagonal top-left to bottom-right
     [2, 4, 6], // diagonal top-right to bottom-left
